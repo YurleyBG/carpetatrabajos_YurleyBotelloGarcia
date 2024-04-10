6de0d2ecb976126f3fe4a5dc160214e4ec51a46c
yurleybotello <yurleybgarcia20@gmail.com>
2024-04-10 12:59:53 -0500

    2commit_python

---
+++ python_BotelloYurleY/Dia2/ejercicio1_YurleyBotelloGarcia.py
@@ -0,0 +1,18 @@
+print("-------------------------------------")
+print("Bienvenido al programa de fibonacci")
+print("-------------------------------------")
+
+print(" la secuencia de fibonacci se encarga de sumar los numeros con el anterior y asi nos dara el siguiente")
+
+num=input("\ningrese un valor : ")
+
+print(num)
+
+for i  in range(num,11,1):
+    num=num+1
+
+    print(num)
+
+
+
+
