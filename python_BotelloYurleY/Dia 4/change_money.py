5c6136e767b78528410162e578cc2c64c5fb19bd
yurleybotello <yurleybgarcia20@gmail.com>
2024-04-12 12:52:14 -0500

    commit4

---
+++ python_BotelloYurleY/Dia 4/change_money.py
@@ -0,0 +1,36 @@
+ 
+print("...........................................................")
+print("_________________-WELCOME MONEY CHANGE_____________________")
+print("...........................................................")
+
+
+coins_i=int(input("\ningrese su dinero porfavor : "))
+
+coins1=1
+cois=5
+coins=10
+
+cont=0
+
+for mon in range(coins_i) :
+  cont +=1
+    
+c=(coins_i//coins)#me indica la cantidad de monedas que necesita de 10 pesos
+print(c)
+
+        
+coi=coins_i//cois#me indica la cantidad de monedas que necesito de 5 pesos
+print(coi)
+    
+co=coins_i//coins1#me indica la cantidad de  monedas que necesita de 1 peso
+print(co)
+
+
+
+
+  
+
+ 
+
+
+  
